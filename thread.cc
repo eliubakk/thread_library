e3be867f71c9ef6a762ee39846eb442b406951c0
Nick Weber <webenic3@gmail.com>
2018-01-30 18:36:23 -0500

    Starter files, no code yet

--- thread.cc
+++ thread.cc
@@ -1,15 +1,5 @@
-//
-//  main.cpp
-//  p2_threadLibrary
-//
-//  Created by Nick Weber on 1/30/18.
-//  Copyright © 2018 Nick Weber. All rights reserved.
-//
-
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    
 }
