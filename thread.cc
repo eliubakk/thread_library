43a968826113010a5ab3f078d32b3ed9150a0fb3
Nick Weber <webenic3@gmail.com>
2018-01-31 18:39:21 -0500

    Starting thread::thread()

--- thread.cc
+++ thread.cc
@@ -1,10 +1,21 @@
 #include "thread.h"
+#include "thread_impl.h"
+#include "thread_globals.h"
 #include <cassert>
 
 using namespace std;
 
-thread::thread(thread_startfunc_t, void *){
-	assert(false);
+thread::thread(thread_startfunc_t func, void *arg){
+	impl_ptr = new impl(); 
+	char *stack = new char [STACK_SIZE];
+	impl_ptr->context->uc_stack.ss_sp = stack;
+	impl_ptr->context->uc_stack.ss_size = STACK_SIZE;
+	impl_ptr->context->uc_stack.ss_flags = 0;
+	impl_ptr->context->uc_link = nullptr;
+
+	makecontext(impl_ptr, func, 1, arg);
+	thread_ready_queue.push(impl_ptr);
+
 } // create a new thread
 thread::~thread(){
 	assert(false);
