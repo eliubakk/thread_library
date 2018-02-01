48f0b8bb0620e3ecaa4078a7c84741c0dadbfb7b
choichan <choichan@umich.edu>
2018-01-31 20:25:13 -0500

    changed test_no_thread

--- test_no_thread.cc
+++ test_no_thread.cc
@@ -1,5 +1,19 @@
 #include "thread.h"
+#include <iostream>
+using namespace std;
+void test_child_thread(void *a){
+	cout << "child is done!" << endl;
+}
+void test_parent_thread(void *a){
+	int *arg = (int *)a;
+	cout << arg << endl;
+	//thread t1((thread_startfunc_t)test_child_thread, (void *)100);
+	//cout << "parent works!" << endl;
+	//t1.yield();
+	//cout << "parent continues!" << endl;
+}
 
-int main(void){
-	cpu::boot(1, nullptr, nullptr, 0, 1, 0);
+int main(){
+	cpu::boot(1, (thread_startfunc_t)test_parent_thread ,(void *)100, 0, 0, 0);
+	return 0;
 }
