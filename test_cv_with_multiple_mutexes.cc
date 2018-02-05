699b197ab95ace6e6b4fcee897a1702db3670807
Nick Weber <webenic3@gmail.com>
2018-02-05 18:58:35 -0500

    Test case - Calling wait() on same CV with multiple mutexes

---
+++ test_cv_with_multiple_mutexes.cc
@@ -0,0 +1,37 @@
+#include "thread.h"
+#include <iostream>
+
+using namespace std;
+
+mutex mutex1;
+cv cv1;
+int counter = 0;
+
+int counter = 0;
+void test_child_thread(void *a){
+	++counter;
+	cout << "child is about to signal without locking mutex" << endl;
+	cv1.signal();
+	cout << "child signaled without locking!" << endl;
+    mutex1.lock();
+    int *arg = (int *)a;
+    cout << "child " << *arg << " is done!" << endl;
+    mutex1.unlock();
+}
+void test_parent_thread(void *a){
+    mutex1.lock();
+    cout << "parent created!" << endl;
+    thread t1((thread_startfunc_t)test_child_thread, a);
+    cout << "parent is now waiting for signal" << endl;
+    while (counter != 0){
+        cv1.wait(mutex1);
+    }
+    cout << "parent is finishing" << endl;
+    mutex1.unlock();
+}
+
+int main(){
+	int num = 100;
+	cpu::boot(1, (thread_startfunc_t)test_parent_thread ,(void *)&num, 0, 0, 2);
+	return 0;
+}
