5d3e349836b6af83f3b274612941f50a10fd2639
choichan <choichan@umich.edu>
2018-02-07 17:24:01 -0500

    updated test_one_thread_at_a_time test case

--- test_one_thread_at_a_time.cc
+++ test_one_thread_at_a_time.cc
@@ -3,24 +3,16 @@
 using namespace std;
 
 mutex mutex1;
-int switch1 = 0;
-
 
 void test_child_thread(void *a){
 	mutex1.lock();
-	cout << "a child thread has been called" << endl;
-	cout << "child is done!" << endl;
-	switch1 = 1;
 	mutex1.unlock();
 }
 void test_parent_thread(void *a){
-	int *arg = (int *)a;
-	cout << *arg << endl;
 	cout << "parent is called" << endl;
-	for (int i = 0; i < 200; ++i) {
-		switch1 = 0;
+	for (int i = 0; i < 100000; ++i) {
 		thread t1((thread_startfunc_t)test_child_thread, a);
-		while (switch1 == 0) {}
+		t1.join();
 	}
 	cout << "parent is finished" << endl;
 }
