24cd7ce6f021eb34f0f94fe2bd4f5a3a28bb78e5
Nick Weber <webenic3@gmail.com>
2018-02-09 21:44:00 -0500

    Bad alloc test case

---
+++ test_bad_alloc.cc
@@ -0,0 +1,25 @@
+#include "thread.h"
+#include <iostream>
+using namespace std;
+
+mutex mutex1;
+cv cv1;
+
+void test_child_thread(void *a){
+	mutex1.lock();
+	cv1.wait(mutex1);
+	mutex1.unlock();
+}
+void test_parent_thread(void *a){
+	cout << "parent is called" << endl;
+	for (int i = 0; i < 100000; ++i) {
+		thread t1((thread_startfunc_t)test_child_thread, a);
+	}
+	cout << "parent is finished" << endl;
+}
+
+int main(){
+	int num = 100;
+	cpu::boot(1, (thread_startfunc_t)test_parent_thread ,(void *)&num, 0, 0, 0);
+	return 0;
+}
